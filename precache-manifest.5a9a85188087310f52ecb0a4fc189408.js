4f09bcf8644cb310810a36ac37de565d7ae19219
pamccombs <pa.mccombs1@gmail.com>
2019-12-29 05:54:56 -0500

    Updates

---
+++ precache-manifest.5a9a85188087310f52ecb0a4fc189408.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39122842f6e2fe61bace4d3a6267ba9e",
+    "url": "/betterofferletter/index.html"
+  },
+  {
+    "revision": "62fc14aa2947e1a39c3d",
+    "url": "/betterofferletter/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d1866db43c565b2aa51d",
+    "url": "/betterofferletter/static/js/2.3b49aa70.chunk.js"
+  },
+  {
+    "revision": "62fc14aa2947e1a39c3d",
+    "url": "/betterofferletter/static/js/main.7908a180.chunk.js"
+  },
+  {
+    "revision": "3e7f1f388f3ff90c4ac3",
+    "url": "/betterofferletter/static/js/runtime-main.ee17983b.js"
+  }
+]);
