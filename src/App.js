94fc0ec6ebf33778bfbd1b4f2d1b5f580d87ee20
pamccombs <34119196+pamccombs@users.noreply.github.com>
2020-01-20 16:46:48 -0500

    add footer to App

--- src/App.js
+++ src/App.js
@@ -3,10 +3,21 @@ import './App.css';
 import { UserForm } from './components/UserForm';
 
 
+
+
 function App() {
   return (
     <div className="App">
       <UserForm />
+      {/* <footer
+      alignText = ''
+      fontSize = {12}>
+      <p>a work worth doing project by Patrick "Reese" McCombs and Matt Wallaert. </p>
+
+<p>OfferLetter.com is a start but pay equity is a much larger topic.  See SalaryOrEquity.com 
+and GetRaised.com for other tools that employees use to to help close the wage gap. </p>
+
+      </footer> */}
     </div>
   );
 }
