18bb2a451c4f54d35934f9b7b8d85f4a41a4bff2
pamccombs <34119196+pamccombs@users.noreply.github.com>
2020-03-03 12:27:01 -0500

    create slide for intro

---
+++ src/components/Intro.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Intro = () => {
+    return (
+        <div className = 'yellowBody'>
+            <p>Hello World</p>
+        </div>
+    )
+}
+
+export default Intro
