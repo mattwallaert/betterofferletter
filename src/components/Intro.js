9814417dfb7d07add73665b2f639e133f4fdba8f
pamccombs <pa.mccombs1@gmail.com>
2020-04-01 10:50:11 -0400

    edit styling

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,18 +3,11 @@ import React from 'react'
 
 const Intro = () => {
     return (
-        <div 
-        className='intro'
-        style={{
-            backgroundColor: '#fdce16',
-            width: window.innerWidth,
-            height: window.innerHeight,
-            justifyContent: 'center'
-            
-          }}
-        
-        >
-            <p>Better Offer Letter</p>
+        <div className='yellow-square-container'>
+            <div
+                className='intro'>
+                <h1>BETTER OFFER LETTER</h1>
+            </div>
         </div>
     )
 }
