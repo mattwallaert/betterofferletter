84b9311d010992dc43c040b050ccd01c4c06b4cd
pamccombs <pa.mccombs1@gmail.com>
2020-04-01 15:58:57 -0400

    correctly added css text center

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,14 +1,12 @@
 import React from 'react'
 
-
 const Intro = () => {
     return (
-        <div className='yellow-square-container'>
             <div
                 className='intro'>
                 <h1>BETTER OFFER LETTER</h1>
+                
             </div>
-        </div>
     )
 }
 
