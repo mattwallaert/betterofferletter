c7e24fa43b8bb69c46c12fc7c39b441feca5a48c
pamccombs <34119196+pamccombs@users.noreply.github.com>
2020-04-06 12:41:31 -0400

    consider background color in App

--- src/App.js
+++ src/App.js
@@ -5,11 +5,44 @@ import { UserForm } from './components/UserForm';
 
 
 
-function App() {
-  return (
-    <div className="App">
-      <UserForm />
-      {/* <footer
+class App extends React.Component {
+
+  state = {
+    color: "Purple"
+  }
+
+  onChangeHandler = e => {
+
+    if (this.state.color === "Purple") {
+
+      this.setState({
+        color : "White"
+      })
+    }
+    if (this.state.color === "White") {
+
+      this.setState({
+        color : "Purple"
+      })
+    }
+  }
+
+
+
+
+
+
+  render() {
+    // const styleObj = {
+    //   background: this.state.color
+    // }
+    return (
+      <div
+        className="App"
+        // style={ styleObj }
+      >
+        <UserForm />
+        {/* <footer
       alignText = ''
       fontSize = {12}>
       <p>a work worth doing project by Patrick "Reese" McCombs and Matt Wallaert. </p>
@@ -18,8 +51,9 @@ function App() {
 and GetRaised.com for other tools that employees use to to help close the wage gap. </p>
 
       </footer> */}
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
