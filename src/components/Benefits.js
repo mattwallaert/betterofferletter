8f1d63a243fdb76286e76d1f6c36bc519b39c105
pamccombs <34119196+pamccombs@users.noreply.github.com>
2020-04-07 12:24:32 -0400

    add save button

--- src/components/Benefits.js
+++ src/components/Benefits.js
@@ -4,6 +4,9 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import TextField from 'material-ui/TextField'
 // import {List, ListItem} from 'material-ui/List'
 import RaisedButton from 'material-ui/RaisedButton'
+import Button from '@material-ui/core/Button';
+// import Icon from '@material-ui/core/Icon';
+import SaveIcon from '@material-ui/icons/Save';
 
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
@@ -11,7 +14,7 @@ import Checkbox from '@material-ui/core/Checkbox';
 import { withStyles } from '@material-ui/core/styles';
 
 export class Benefits extends Component {
-    
+
 
     continue = e => {
         e.preventDefault();
@@ -33,126 +36,135 @@ export class Benefits extends Component {
                     <p>
                         Check all that apply.
                     </p>
-    
+
                     {/* <p><input type="checkbox" name="health-insurance" /> Health Insurance</p> */}
 
                     <FormGroup className={classes.root}>
                         <FormControlLabel
                             control={
-                            <Checkbox
-                                checked={values.health_insurance}
-                                onChange={handleBenefitsCheck('health_insurance')}
-                                // value= 'health_insurance'
-                                color="primary"
-                            />
+                                <Checkbox
+                                    checked={values.health_insurance}
+                                    onChange={handleBenefitsCheck('health_insurance')}
+                                    // value= 'health_insurance'
+                                    color="primary"
+                                />
                             } label="Health Insurance"
                         />
-                         <FormControlLabel
+                        <FormControlLabel
                             control={
-                            <Checkbox
-                                checked={values.dental_insurance}
-                                onChange={handleBenefitsCheck('dental_insurance')}
-                                // value= 'dental_insurance'
-                                color="primary"
-                            />
+                                <Checkbox
+                                    checked={values.dental_insurance}
+                                    onChange={handleBenefitsCheck('dental_insurance')}
+                                    // value= 'dental_insurance'
+                                    color="primary"
+                                />
                             } label="Dental Insurance"
                         />
                         <FormControlLabel
                             control={
-                            <Checkbox
-                                checked={values.vision_insurance}
-                                onChange={handleBenefitsCheck('vision_insurance')}
-                                // value= 'vision_insurance'
-                                color="primary"
-                            />
+                                <Checkbox
+                                    checked={values.vision_insurance}
+                                    onChange={handleBenefitsCheck('vision_insurance')}
+                                    // value= 'vision_insurance'
+                                    color="primary"
+                                />
                             } label="Vision Insurance"
                         />
-                         <FormControlLabel
+                        <FormControlLabel
                             control={
-                            <Checkbox
-                                checked={values.pre_tax_spending}
-                                onChange={handleBenefitsCheck('pre_tax_spending')}
-                                // value= 'pre_tax_spending'
-                                color="primary"
-                            />
+                                <Checkbox
+                                    checked={values.pre_tax_spending}
+                                    onChange={handleBenefitsCheck('pre_tax_spending')}
+                                    // value= 'pre_tax_spending'
+                                    color="primary"
+                                />
                             } label="Pre-Tax Spending Accounts"
                         />
                         <FormControlLabel
                             control={
-                            <Checkbox
-                                checked={values.retirement_matching}
-                                onChange={handleBenefitsCheck('retirement_matching')}
-                                // value= 'retirement_matching'
-                                color="primary"
-                            />
+                                <Checkbox
+                                    checked={values.retirement_matching}
+                                    onChange={handleBenefitsCheck('retirement_matching')}
+                                    // value= 'retirement_matching'
+                                    color="primary"
+                                />
                             } label="Retirement Matching"
                         />
                         <FormControlLabel
                             control={
-                            <Checkbox
-                                checked={values.tuition_reimbursement}
-                                onChange={handleBenefitsCheck('tuition_reimbursement')}
-                                // value= 'tuition_reimbursement'
-                                color="primary"
-                            />
+                                <Checkbox
+                                    checked={values.tuition_reimbursement}
+                                    onChange={handleBenefitsCheck('tuition_reimbursement')}
+                                    // value= 'tuition_reimbursement'
+                                    color="primary"
+                                />
                             } label="Tuition Reimbursement"
                         />
                         <FormControlLabel
                             control={
-                            <Checkbox
-                                checked={values.childcare}
-                                onChange={handleBenefitsCheck('childcare')}
-                                // value= 'childcare'
-                                color="primary"
-                            />
+                                <Checkbox
+                                    checked={values.childcare}
+                                    onChange={handleBenefitsCheck('childcare')}
+                                    // value= 'childcare'
+                                    color="primary"
+                                />
                             } label="Childcare Benefits"
                         />
                         <FormControlLabel
                             control={
-                            <Checkbox
-                                checked={values.wellness}
-                                onChange={handleBenefitsCheck('wellness')}
-                                // value= 'wellness'
-                                color="primary"
-                            />
+                                <Checkbox
+                                    checked={values.wellness}
+                                    onChange={handleBenefitsCheck('wellness')}
+                                    // value= 'wellness'
+                                    color="primary"
+                                />
                             } label="Wellness Benefits"
                         />
                         <FormControlLabel
                             control={
-                            <Checkbox
-                                checked={values.lunch}
-                                onChange={handleBenefitsCheck('lunch')}
-                                // value= 'lunch'
-                                color="primary"
-                            />
+                                <Checkbox
+                                    checked={values.lunch}
+                                    onChange={handleBenefitsCheck('lunch')}
+                                    // value= 'lunch'
+                                    color="primary"
+                                />
                             } label="Free Lunch"
                         />
                         <FormControlLabel
                             control={
-                            <Checkbox
-                                checked={values.student_loan_assist}
-                                onChange={handleBenefitsCheck('student_loan_assist')}
-                                // value= 'student_loan_assist'
-                                color="primary"
-                            />
+                                <Checkbox
+                                    checked={values.student_loan_assist}
+                                    onChange={handleBenefitsCheck('student_loan_assist')}
+                                    // value= 'student_loan_assist'
+                                    color="primary"
+                                />
                             } label="Student Loan Assistance"
                         />
                     </FormGroup>
-                    <TextField 
-                            hintText="Other Benefit"
-                            floatingLabelText="Other Benefit"
-                            onChange={handleChange('other_benefit')}
-                            defaultValue={values.other_benefit}
-                        /> 
-              
-                    <br/>
-                    <RaisedButton 
+                    <TextField
+                        hintText="Other Benefit"
+                        floatingLabelText="Other Benefit"
+                        onChange={handleChange('other_benefit')}
+                        defaultValue={values.other_benefit}
+                    />
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        size="small"
+                        className={classes.button}
+                        startIcon={<SaveIcon />}
+                    >
+                        Save
+                    </Button>
+
+                    <br />
+                    <RaisedButton
                         label="Back"
                         primary={false}
                         style={styles.button}
                         onClick={this.back}
                     />
-                    <RaisedButton 
+                    <RaisedButton
                         label="Continue"
                         primary={true}
                         style={styles.button}
@@ -174,7 +186,7 @@ const styles = {
         flexDirection: 'row',
         flexWrap: 'wrap',
         justifyContent: "center"
-        
+
     }
 }
 
